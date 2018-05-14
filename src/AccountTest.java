332a683f9c5f740878b0fef78d7ec549e25efc8d
300911292 <earldenzel@gmail.com>
2018-05-14 17:49:00 -0400

    [ADD] Account class [ADD] AccountTest driver class

--- src/AccountTest.java
+++ src/AccountTest.java
@@ -10,5 +10,14 @@ public class AccountTest {
 
         //Display default account info
         System.out.printf("Initial name is %s%n%n",account.getAccountName());
+
+        //Prompt new name
+        System.out.println("Please enter an account name: ");
+        String accName = input.nextLine();
+        account.setAccountName(accName);
+
+        //Display info
+        System.out.printf("New name is %s%n%n",account.getAccountName());
+
     }
 }
