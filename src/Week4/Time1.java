b2bc44509f0a097dbeef1882731597df0f7452ef
Earl Perez <earldenzel@gmail.com>
2018-05-28 18:20:46 -0400

    Added examples 5, 6 and 7

---
+++ src/Week4/Time1.java
@@ -0,0 +1,35 @@
+package Week4;
+
+//Define time in a 24-hour format, but accommodate 12 hours
+public class Time1 {
+    private int hour;
+    private int minute;
+    private int second;
+
+    //ability to set time
+    public void setTime(int hour, int minute, int second){
+        if(hour < 0 || hour >24 ||
+                minute < 0 || minute > 60 ||
+                second < 0 || second > 60)
+        {
+            throw new IllegalArgumentException("Hour, minute, and/or second was out of range");
+        }
+
+        //assuming the value are correct
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public String toUniversalString(){
+        return String.format("%02d:%02d:%02d", hour, minute, second);
+    }
+
+    public String toString(){
+        return String.format("%d:%02d:%02d %s",
+                ((hour == 0 || hour == 12) ? 12 : hour % 12),
+                minute,
+                second,
+                ((hour > 12) ? "AM" : "PM"));
+    }
+}
